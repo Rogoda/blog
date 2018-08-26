ad68ceb84ef1f828a0cc98657504a80374e8b7c5
Rogoda <rogoda0@gmail.com>
2018-08-26 10:06:18 +0200

    search optimization

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 Write-Host Starting site at development mode
 Write-Host site will be hosted on localhost:4000
 Write-Host site contains drafts
-bundle exec jekyll serve --drafts
+bundle exec jekyll serve --config _config.yml,_config.dev.yml --drafts
 Write-Host hosting has been terminated
